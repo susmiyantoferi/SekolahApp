a06300ff81d060f8bec5bef9f35cf4fd9b712fc5
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2021-07-21 21:49:20 +0700

    #7 Join Table Dan Ditampilkan Ke Halaman All Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     protected $fillable = [
         'user_id', 'category_name',
     ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
