56570a322cd6792bbd2a3b5d79bdc4bf296645b6
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2021-07-29 20:48:22 +0700

    #17 Email Verification

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use App\Http\Controllers\BrandController;
 |
 */
 
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
 Route::get('/', function () {
     return view('welcome');
 });
